f93f9484a5a4a54ac857e842313936ecfb01d97d
Deivid Alan <deividalan@hotmail.com>
2024-09-14 16:15:44 -0300

    add login route

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask
-from database import db
+from flask import Flask, request, jsonify
 from models.user import User
+from database import db
+from flask_login import LoginManager, login_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
+login_manager = LoginManager()
+
 db.init_app(app)
+login_manager.init_app(app)
+
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter(User.username == username).first()
+
+        if user and user.password == password:
+            login_user(user)
+            return jsonify("message: user authorized!")
+    
+    return jsonify("message: username and password is required!"), 400
 
 @app.route("/hello-word", methods=["GET"])
 def hello_word():
