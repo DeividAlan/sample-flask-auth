e0a9b595378be117f5ca116fd96520305350e413
Deivid Alan <deividalan@hotmail.com>
2024-09-14 16:28:15 -0300

    add logout rote

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -32,6 +32,12 @@ def login():
             return jsonify("message: user authorized!")
     
     return jsonify("message: username and password is required!"), 400
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify("message: You have been successfully logged out.")
 
 @app.route("/hello-word", methods=["GET"])
 def hello_word():
